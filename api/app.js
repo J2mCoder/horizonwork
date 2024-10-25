7581051f4c4406cd810c0a6a9bb70deaf7e92738
J2mCoder <jeandedieumbumba2802@gmail.com>
2024-10-25 20:48:12 +0100

    fix: update

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ app.use(urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://horizonworks.vercel.app/"],
+    origin: ["http://localhost:5173", "https://horizonworks.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Origin",
